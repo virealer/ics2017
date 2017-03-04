d349b36adbd7abd5b216007f1813bac746590482
tracer-ics2017 <tracer@njuics.org>
2017-03-04 11:00:03 +0000

    >  compile NEMU caopenghui ics Linux 5fa923efe61d 4.9.8-moby #1 SMP Wed Feb 8 09:56:43 UTC 2017 x86_64 GNU/Linux  11:00:03 up 5 days, 18:42,  3 users,  load average: 0.15, 0.04, 0.01 91400091af4c535c55256ef9d6f2598855dbf8fc

---
+++ temp/union.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+struct test{
+	union{
+	unsigned int f[4];
+	struct{
+		unsigned int a, b, c, d;
+		};
+	};
+	unsigned int e;
+};
+int main(){
+	struct test t1={1, 2, 3, 4};
+	printf("%d\n", sizeof(t1)); 
+	printf("%d, %d, %d, %d, %d", t1.a, t1.b, t1.c, t1.d, t1.e);
+	printf("%d\n", sizeof(int));
+}
