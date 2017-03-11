edc48579e27bf06190524ae806c93806e9815dac
tracer-ics2017 <tracer@njuics.org>
2017-03-11 06:28:08 +0000

    >  compile NEMU caopenghui ics Linux b4a3e7dd4bfe 4.9.8-moby #1 SMP Wed Feb 8 09:56:43 UTC 2017 x86_64 GNU/Linux  06:28:07 up 1 day,  5:41,  2 users,  load average: 0.00, 0.02, 0.00 c750c444ee982f733e9b54d6d518a8a8143e018e

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -84,7 +84,7 @@ static bool make_token(char *e) {
 				 */
 
 				switch(rules[i].token_type) {
-					case 0:
+					case NOTYPE:
 						break;
 					default:
 						tokens[nr_token].type = rules[i].token_type;
