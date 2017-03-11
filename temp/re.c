776270ca0093f126ddc6e2bb2848aa6aeec6b550
tracer-ics2017 <tracer@njuics.org>
2017-03-11 08:42:07 +0000

    >  run caopenghui ics Linux b4a3e7dd4bfe 4.9.8-moby #1 SMP Wed Feb 8 09:56:43 UTC 2017 x86_64 GNU/Linux  08:42:07 up 1 day,  7:55,  2 users,  load average: 0.02, 0.08, 0.04 911f7b54bf22795d43e68d6189d390274fa9efac

--- temp/re.c
+++ temp/re.c
@@ -7,3 +7,42 @@ int main(){
 }
 
 
+static int eval(p, q) {
+    if(p > q) {
+        /* Bad expression */
+        return 0;
+    }
+    else if(p == q) { 
+        /* Single token.
+         * For now this token should be a number. 
+         * Return the value of the number.
+         */ 
+        if(tokens[p].type != NUM){
+            puts("wrong ...");
+            return 0;
+        }
+        int n = atoi(tokens[p].str);
+        return n;
+    }
+    else if(check_parentheses(p, q) == true) {
+        /* The expression is surrounded by a matched pair of parentheses. 
+         * If that is the case, just throw away the parentheses.
+         */
+        return eval(p + 1, q - 1); 
+    }
+    else {
+        /* We should do more things here. */
+//		op = the position of dominant operator in the token expression;
+        int op = 5;
+        int val1 = eval(p, op - 1);
+        int val2 = eval(op + 1, q);
+
+        switch(op) {
+            case '+': return val1 + val2;
+            case '-': return val1 - val2;
+            case '*': return val1 * val2;
+            case '/': return val1 / val2;
+            default: assert(0);
+        }
+    }
+}
