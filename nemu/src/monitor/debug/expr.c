2569486ce944de077e22886dbe6150d8895b9e4a
tracer-ics2017 <tracer@njuics.org>
2017-03-11 06:48:25 +0000

    >  compile NEMU caopenghui ics Linux b4a3e7dd4bfe 4.9.8-moby #1 SMP Wed Feb 8 09:56:43 UTC 2017 x86_64 GNU/Linux  06:48:25 up 1 day,  6:02,  2 users,  load average: 0.00, 0.00, 0.00 4832bf58e7ebc71949eb5cdabbac36ae6b2be5af

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -28,7 +28,7 @@ static struct rule {
 	{"-", '-'},						// minus
 	{"\\*", '*'},					// multiply
 	{"/", '/'},						// devide
-	{"\\d{1,32}", '0'}   				// int
+	{"\\[0-9]{1,32}", '0'}   				// int
 };
 
 #define NR_REGEX (sizeof(rules) / sizeof(rules[0]) )
