36b3bdd569b4c9404fa32ccb8d278963af105ff9
tracer-ics2017 <tracer@njuics.org>
2017-03-21 05:10:03 +0000

    >  compile NEMU caopenghui ics Linux b4a3e7dd4bfe 4.9.12-moby #1 SMP Tue Feb 28 12:11:36 UTC 2017 x86_64 GNU/Linux  05:10:03 up 4 days, 20:29,  2 users,  load average: 0.08, 0.04, 0.01 b9424b7becc2babf75be599d428d5ff5b0759ba9

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -58,21 +58,15 @@ bool check_wp(){
 	WP* tem = head;
 	int new_val = 0;
 	bool* success = false;
-//	do {
-//		new_val = expr(tem->expr, success);
-//		if(new_val != tem->pre_val){
-//			printf("pre:%d\nnew:%d\n", tem->pre_val, new_val);
-//			tem->pre_val = new_val;
-//			return false;
-//		} 
-//		tem = tem->next;
-//	}while(tem);
-	new_val = expr(tem->expr, success);
-	if(new_val != tem->pre_val){
-		printf("pre:%d\nnew:%d\n", tem->pre_val, new_val);
-		tem->pre_val = new_val;
-		return false;
-	} 
+	while(tem){
+		new_val = expr(tem->expr, success);
+		if(new_val != tem->pre_val){
+			printf("pre:%d\nnew:%d\n", tem->pre_val, new_val);
+			tem->pre_val = new_val;
+			return false;
+		} 
+		tem = tem->next;
+	};
 	tem = tem->next;
 	printf("new:%d\n", new_val);
 	return true;
